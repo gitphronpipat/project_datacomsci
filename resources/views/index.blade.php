a66c26e1bac4be4e69915c46c53eef0db08909d3
gitphronpipat <phronpipat.phromsapsak@gmail.com>
2026-09-17 21:48:44 +0700

    add for test admin add and component2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="th">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <h1>maleeeieiza</h1>
+    <title>หน้าแรก</title>
     @include('theme.component')
 </head>
+<body>
+
+    <h1>maleeeieiza</h1>
+
+</body>
+</html>
